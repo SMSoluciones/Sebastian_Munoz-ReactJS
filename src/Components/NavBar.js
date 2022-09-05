42907f3a279040785bc7e76b8b1517773be52f32
Sebastian Munoz <smunoz.id@gmail.com>
2022-09-05 17:19:50 -0300

    Desafio Menu E-Commerce

---
+++ src/Components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import "./NavBar.css";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="contacto_directo">
+        <InstagramIcon fontSize="medium" />
+        <WhatsAppIcon fontSize="medium" />
+      </div>
+
+      <header>
+        <img className="logo" src={logo} alt="" />
+        <nav>
+          <ul>
+            <a href="http://">
+              <li>PASTAS</li>
+            </a>
+            <a href="http://">
+              <li>TARTAS</li>
+            </a>
+            <a href="http://">
+              <li>EMPANADAS</li>
+            </a>
+            <a href="http://">
+              <li>CONTACTO</li>
+            </a>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
