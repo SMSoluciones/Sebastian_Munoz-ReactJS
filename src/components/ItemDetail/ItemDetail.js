affabed3dbeeefea1ec480701f2cd634e9d3f5bf
Sebastian <smunoz.id@gmail.com>
2022-10-03 14:25:49 -0300

    Desafio Cart View sin CSS Rev02

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { useCartContext } from "../../Context/CartContext";
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
+import styled from "styled-components";
 
 export const ItemDetail = ({ item }) => {
   const [terminarCompra, setTerminarCompra] = useState(false);
@@ -21,21 +22,33 @@ export const ItemDetail = ({ item }) => {
     }).showToast();
   };
   return (
-    <div className="itemDetail">
-      <img src={item.image} alt="" />
-      <div className="itemInformation">
-        <h2>{item.title}</h2>
-        <p>{item.description}</p>
-        <p className="price">$ {item.price}</p>
+    <>
+      <Link to="/">
+        <Volver>Volver</Volver>
+      </Link>
+      <div className="itemDetail">
+        <img src={item.image} alt="" />
+        <div className="itemInformation">
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          <p className="price">$ {item.price}</p>
 
-        {terminarCompra ? (
-          <Link to="/cart">
-            <button>Finalizar Compra</button>
-          </Link>
-        ) : (
-          <ItemCount inicio={1} stock={10} onAdd={onAdd} />
-        )}
+          {terminarCompra ? (
+            <Link to="/cart">
+              <button>Finalizar Compra</button>
+            </Link>
+          ) : (
+            <ItemCount inicio={1} stock={10} onAdd={onAdd} />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
+const Volver = styled.button`
+  position: absolute;
+  background-color: black;
+  color: white;
+  width: 100px;
+`;
