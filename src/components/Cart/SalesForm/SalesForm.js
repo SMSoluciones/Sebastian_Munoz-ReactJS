6a4d2854b6244b7120c4090dafb8f019d8608c09
Sebastian Munoz <smunoz.id@gmail.com>
2022-10-13 00:30:24 -0300

    Formulario con logica creada

--- src/components/Cart/SalesForm/SalesForm.js
+++ src/components/Cart/SalesForm/SalesForm.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../../firebase/firebase";
 import { useCartContext } from "../../../Context/CartContext";
 
 export const SalesForm = () => {
   const { cart, totalPrice, clear } = useCartContext();
+  const { purchaseId, setPurchaseId } = useState("");
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    adress: "",
+    flat: "",
+  });
   const total = totalPrice();
 
-  const datosComprador = {
-    nombre: "Sebastian",
-    apellido: "Muñoz",
-    edad: 32,
-    card: 123184124123,
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues({ ...inputValues, [name]: value });
   };
 
-  const finalizarCompra = () => {
+  const finalSale = (e) => {
+    e.preventDefault();
     const ventasCollection = collection(db, "sales");
     addDoc(ventasCollection, {
-      datosComprador,
+      usuario: inputValues,
       items: cart,
       date: serverTimestamp(),
       total,
@@ -29,9 +37,48 @@ export const SalesForm = () => {
 
   return (
     <>
-      <h1> $ {totalPrice()} </h1>
-      <form action=""></form>
-      <button onClick={finalizarCompra}>EnviarCompra</button>;
+      <h1>Usted debera abonar: $ {totalPrice()} </h1>
+      <div>
+        <form onSubmit={finalSale}>
+          <input
+            name="name"
+            type="name"
+            placeholder="Nombre"
+            onChange={handleOnChange}
+          />
+          <input
+            name="surname"
+            type="family-name"
+            placeholder="Apellido"
+            onChange={handleOnChange}
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={handleOnChange}
+          />
+          <input
+            name="phone"
+            type="tel"
+            placeholder="Telefono"
+            onChange={handleOnChange}
+          />
+          <input
+            name="adress"
+            type="street-address"
+            placeholder="Direccion"
+            onChange={handleOnChange}
+          />
+          <input
+            name="flat"
+            type="text"
+            placeholder="Departamento"
+            onChange={handleOnChange}
+          />
+          <button>Finalizar Compra</button>
+        </form>
+      </div>
     </>
   );
 };
